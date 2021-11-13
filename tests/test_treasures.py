8a752dfe5fa6acdbf8178a81bf11c8e8365dd4e8
Austin Skoog <fredyybob@gmail.com>
2021-11-13 11:09:28 -0600

    ring of revenge test added

--- tests/test_treasures.py
+++ tests/test_treasures.py
@@ -12,9 +12,27 @@ def test_treasure(treasure, mimic):
                                   tribes=[tribe for tribe in Tribe])
     player = make_player(
         characters=[generic_char],
+        treasures=[treasure]
     )
     enemy = make_player(
         characters=[make_character(id='SBB_CHARACTER_MONSTAR', position=1, attack=1, health=1)],
     )
     board = Board({'PLAYER': player, 'ENEMY': enemy})
     winner, loser = board.fight()
+
+def test_ring_of_revenge():
+    peep = make_character(id='SBB_CHARACTER_PRINCESSPEEP', attack=1, health=1, position=1)
+    generic_char1 = make_character(id='GENERIC', attack=1, health=1, position=2)
+    generic_char2 = make_character(id='GENERIC', attack=1, health=1, position=3)
+
+    player = make_player(
+        characters=[peep, generic_char1,generic_char2],
+        treasures=['SBB_TREASURE_RINGOFREVENGE','SBB_TREASURE_HERMESBOOTS']
+    )
+    enemy = make_player(
+        characters=[make_character(id='SBB_CHARACTER_MONSTAR', position=1, attack=1, health=1)],
+    )
+    board = Board({'PLAYER': player, 'ENEMY': enemy})
+    winner, loser = board.fight()
+    assert player.characters[1].attack == 1
+    assert player.characters[5].attack == 2
