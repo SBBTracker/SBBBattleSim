67afbfdc660302e6d524f66e26de19c68a5d06f7
Reginald <reginaldbeakes@gmail.com>
2021-11-14 14:06:02 -0500

    Adding Pumpkin King

--- sbbbattlesim/characters/SBB_CHARACTER_PUMPKINKING.py
+++ sbbbattlesim/characters/SBB_CHARACTER_PUMPKINKING.py
@@ -1,5 +1,9 @@
+import random
+
 from sbbbattlesim.characters import Character
+from sbbbattlesim.events import OnDeath
 from sbbbattlesim.utils import StatChangeCause, Tribe
+from sbbbattlesim.characters import registry as character_registry
 
 
 class CharacterType(Character):
@@ -9,3 +13,19 @@ class CharacterType(Character):
     _health = 5
     _level = 6
     _tribes = {Tribe.EVIL, Tribe.MONSTER}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        class PumpkinKingOnDeath(OnDeath):
+            def handle(self, *args, **kwargs):
+                summons = []
+                dead_in_order = sorted([char for char in self.manager.graveyard if Tribe.EVIL in char], key=lambda char: char._level, reverse=True)
+                for dead in dead_in_order[:7]:
+                    summons.append(random.choice(character_registry.get(_lambda=lambda char: char._level == dead._level-1 and Tribe.EVIL in char._tribes)).new(
+                        owner=self.manager.owner,
+                        position=self.manager.position,
+                        golden=self.manager.golden
+                    ))
+
+                self.manager.owner.summon(self.manager.position, *summons)
