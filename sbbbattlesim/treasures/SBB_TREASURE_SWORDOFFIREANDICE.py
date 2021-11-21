b9d1941af609eba6a70dc6929b132ac777c4f080
austin <fredyybob@gmail.com>
2021-11-21 10:56:53 -0600

    test/treasure fixes

--- sbbbattlesim/treasures/SBB_TREASURE_SWORDOFFIREANDICE.py
+++ sbbbattlesim/treasures/SBB_TREASURE_SWORDOFFIREANDICE.py
@@ -8,10 +8,10 @@ class TreasureType(Treasure):
 
     def buff(self, target_character):
         if 4 <= target_character.position:
-            for _ in range(self.mimic + 1):
-                target_character.change_stats(health=5, reason=StatChangeCause.SWORD_OF_FIRE_AND_ICE, source=self,
+            for _ in range(bool(self.mimic) + 1):
+                target_character.change_stats(health=6, reason=StatChangeCause.SWORD_OF_FIRE_AND_ICE, source=self,
                                               temp=True)
         else:
-            for _ in range(self.mimic + 1):
-                target_character.change_stats(attack=5, reason=StatChangeCause.SWORD_OF_FIRE_AND_ICE, source=self,
+            for _ in range(bool(self.mimic) + 1):
+                target_character.change_stats(attack=6, reason=StatChangeCause.SWORD_OF_FIRE_AND_ICE, source=self,
                                               temp=True)
