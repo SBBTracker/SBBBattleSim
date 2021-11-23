e88d7c1c38790430835ea9cb2f813ee403355d53
austin <fredyybob@gmail.com>
2021-11-22 18:31:46 -0600

    removed hanging chad of treasure

--- sbbbattlesim/spells/SBB_TREASURE_MERLIN'SHAT.py
+++
@@ -1,9 +0,0 @@
-from sbbbattlesim.spells import TargetedSpell
-from sbbbattlesim.utils import StatChangeCause
-
-
-class SpellType(TargetedSpell):
-    display_name = 'Stoneskin'
-
-    def cast(self, target, *args, **kwargs):
-        target.change_stats(health=4, attack=4, temp=False, reason=StatChangeCause.MERLINS_TEST, source=self)
