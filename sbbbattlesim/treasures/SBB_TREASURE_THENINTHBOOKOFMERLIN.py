397fc9a5d51e737cc89087dce81c50f1691d561e
austin <fredyybob@gmail.com>
2021-11-23 21:49:15 -0600

    things cast spells now

--- sbbbattlesim/treasures/SBB_TREASURE_THENINTHBOOKOFMERLIN.py
+++ sbbbattlesim/treasures/SBB_TREASURE_THENINTHBOOKOFMERLIN.py
@@ -1,8 +1,7 @@
 from sbbbattlesim.treasures import Treasure
 from sbbbattlesim.events import OnDeath
-from sbbbattlesim.utils import COMBAT_SPELLS
+from sbbbattlesim.utils import random_combat_spell
 
-#todo update spell list
 class TreasureType(Treasure):
     display_name = 'The Ninth Book of Merlin'
 
@@ -13,4 +12,8 @@ class TreasureType(Treasure):
             last_breath = True
 
             def handle(self, *args, **kwargs):
-                pass
+                for _ in range(bool(self.book.mimic)+ 1):
+                    random_combat_spell(self.manager.owner.level).cast()
+
+        target_character.register(NinthBookOnDeath)
+        target_character.last_breath = True
