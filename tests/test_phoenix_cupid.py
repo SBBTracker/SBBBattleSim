c046c4176954bc0a501b9b16e17db3487585b8e6
ilyas <ilyas.aricanli@gmail.com>
2021-11-25 03:08:02 -0500

    highly advanced test demonstrating that this works

--- tests/test_phoenix_cupid.py
+++ tests/test_phoenix_cupid.py
@@ -21,3 +21,22 @@ def test_phoenix_cupid():
     assert board.p1.characters[1] is None
     assert board.p1.characters[5] is None
 
+def test_phoenix_bouncy_cupid():
+
+    player = make_player(
+        characters=[
+            make_character(id="SBB_CHARACTER_CUPID", position=5, attack=1, health=500),
+            make_character(position=1, attack=500, health=1),
+        ],
+        treasures=['SBB_TREASURE_PHOENIXFEATHER']
+    )
+    enemy = make_player(
+        characters=[make_character(id="SBB_CHARACTER_CUPID", attack=1, health=1)],
+        treasures=['''SBB_TREASURE_HERMES'BOOTS''']
+    )
+    board = Board({'PLAYER': player, 'ENEMY': enemy})
+    winner, loser = board.fight(limit=1)
+
+    assert board.p1.characters[1] is None
+    assert board.p1.characters[5] is None
+
