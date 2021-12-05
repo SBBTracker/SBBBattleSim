701204560fab98a770fe74c3100a26c9d083f33e
ilyas <ilyas.aricanli@gmail.com>
2021-12-05 14:21:54 -0500

    use merling to verify onstart not proccing spells

---
+++ tests/interactions/test_onstart_notproccing_spell.py
@@ -0,0 +1,29 @@
+import pytest
+
+from sbbbattlesim import Board
+from tests import make_character, make_player
+
+def test_onstart_not_proc_merlin():
+    player = make_player(
+        characters=[
+            make_character(id='GENERIC', position=1)
+        ],
+        hero='SBB_HERO_MERLIN',
+        spells=[
+            'SBB_SPELL_EARTHQUAKE'
+        ]
+    )
+    enemy = make_player(
+        characters=[make_character(id='GENERIC', position=1)],
+    )
+    board = Board({'PLAYER': player, 'ENEMY': enemy})
+    winner, loser = board.fight()
+    board.p1.resolve_board()
+    board.p2.resolve_board()
+
+    player = board.p1
+
+    generic = player.characters.get(1)
+
+    assert generic
+    assert generic.attack == 1 and generic.health == 1
