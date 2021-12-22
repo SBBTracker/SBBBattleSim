391bae8f18eff12c58df223bad76d23c1a62eabc
ilyas <ilyas.aricanli@gmail.com>
2021-12-22 00:49:13 -0500

    grim soul can no longer target southsea siren

--- tests/interactions/test_grim_soul.py
+++ tests/interactions/test_grim_soul.py
@@ -26,3 +26,23 @@ def test_grimsoul(golden):
 
     final_stats = (5, 5) if golden else (3, 3)
     assert (board.p1.characters[2].attack, board.p1.characters[2].health) == final_stats
+
+
+
+def test_grimsoul_southsea():
+    player = make_player(
+        characters=[
+            make_character(id='SBB_CHARACTER_CERBERUS', position=1, attack=1, health=1),
+            make_character(id='SBB_CHARACTER_LOBO', position=2, attack=1, health=1, golden=False),
+        ],
+        treasures=[
+            '''SBB_TREASURE_HERMES'BOOTS'''
+        ]
+    )
+    enemy = make_player(
+        characters=[make_character(attack=1, health=1)],
+    )
+    board = Board({'PLAYER': player, 'ENEMY': enemy})
+    winner, loser = board.fight(limit=1)
+    board.p1.resolve_board()
+    board.p2.resolve_board()
