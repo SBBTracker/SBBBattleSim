acdea7fb9e5f33a2aa1e009115f6a0bf3c049daf
Andrew Rasch <raschykid@gmail.com>
2022-01-24 22:38:51 -0500

    Handle golden faeries base stats

--- sbbbattlesim/characters/SBB_CHARACTER_GOODANDEVILSISTERS.py
+++ sbbbattlesim/characters/SBB_CHARACTER_GOODANDEVILSISTERS.py
@@ -18,10 +18,10 @@ class FairyBuffOnSummon(OnSummon):
             _lambda=lambda char: Tribe.EVIL in char.tribes)] + [0])
         highest_health_good = max(
             [char.health for char in self.source.player.valid_characters() if Tribe.GOOD in char.tribes] + [0])
-
+        multiplier = 2 if self.source.golden else 1
         Buff(reason=ActionReason.SHOULDER_FAIRY_BUFF, source=self.source, targets=[self.source],
-             attack=highest_attack_evil * (2 if self.source.golden else 1) - (1 if highest_attack_evil > 0 else 0),
-             health=highest_health_good * (2 if self.source.golden else 1) - (1 if highest_health_good > 0 else 0),
+             attack=highest_attack_evil * multiplier - (multiplier if highest_attack_evil > 0 else 0),
+             health=highest_health_good * multiplier - (multiplier if highest_health_good > 0 else 0),
              temp=False).resolve()
 
 
